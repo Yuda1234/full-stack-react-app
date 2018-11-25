0c78dff679f0772a11100292fcf1a53bdffb9a83
yehuda <yuda0527@gmail.com>
2018-11-25 12:31:21 +0200

    added backend code

---
+++ server/models/weather.js
@@ -0,0 +1,20 @@
+const request = require('request-promise');
+
+// require('dotenv').config();
+const API_KEY = 'ffaef4cf05a408a94664a643f739b793'
+
+class Weather {
+  static retrieveByCity (city, callback) {
+    request({
+      uri: `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`,
+      json: true
+    }).then((res) => {
+      callback(res);
+    }).catch((err) => {
+      console.log(err);
+      callback({ error: 'Could not reach OpenWeatherMap API.' });
+    });
+  }
+}
+
+module.exports = Weather;
